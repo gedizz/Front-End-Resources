895db6b5997eb9cb9bf0d616bbfebe5ad5507749
Dylan Williams <Dylan.Williams@trojans.dsu.edu>
2022-05-28 10:37:08 -0500

    rainbow effect

---
+++ Incomplete Projects/textContent_lib/content.js
@@ -0,0 +1,18 @@
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+
+const firstH1 = document.querySelector("h1");
+const text = firstH1.textContent;
+firstH1.textContent = "";
+
+
+let currentColor = 0;
+for(const letter of text) {
+  let currentSpan = document.createElement("span");
+  currentSpan.textContent = letter;
+  currentSpan.style.color = colors[currentColor];
+
+  currentColor = (currentColor + 1) % 6;
+
+  firstH1.appendChild(currentSpan);
+}
