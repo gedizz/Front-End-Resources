c97233a9fadd86dd5d0119e12de59b24a32cd43d
gedizzletsplay@gmail.com <Duke1@234>
2022-05-28 02:16:22 +0100

    added flip effect - card front format set

---
+++ Incomplete Projects/Product Element/js/card.js
@@ -0,0 +1,8 @@
+const productCard = document.querySelector(".product-card");
+const infoCard = document.querySelector("card-info");
+
+productCard.addEventListener("mouseover", showSizesColors);
+
+function showSizesColors() {
+    
+}
