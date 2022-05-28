1a75dc1209fedc82ef94b675289ae6678fded4a0
gedizzletsplay@gmail.com <Duke1@234>
2022-05-28 03:32:56 +0100

    card no longer flips - need to add button

--- Incomplete Projects/Product Element/js/card.js
+++ Incomplete Projects/Product Element/js/card.js
@@ -1,7 +1,12 @@
 const productCard = document.querySelector(".product-card");
 const infoCard = document.querySelector("card-info");
 
-productCard.addEventListener("mouseover", showSizesColors);
+productCard.addEventListener("mouseover", showSizesColors, cardHover);
+
+function cardHover() {
+    // add style to card
+    
+}
 
 function showSizesColors() {
     
