53e08f33d70f9feb1be95f82b031035f58956e7e
gedizzletsplay@gmail.com <Duke1@234>
2022-05-29 03:10:37 +0100

    fixed card.js not sliding up

--- Incomplete Projects/Product Element/js/card.js
+++ Incomplete Projects/Product Element/js/card.js
@@ -1,15 +1,23 @@
+const productCard = document.querySelector(".product-card")
+const infoCard = document.getElementById("card-info")
+const sizesColors = document.getElementById("pop-up")
+
 const cardToFlip = document.querySelector(".flip-card")
 const flipButton = document.getElementById("flip-button")
-const exitButton = document.getElementById("exit-button")
 
 const imageContainer = document.querySelector(".image-container")
 
 const leftArrow = document.getElementById("arrow-left")
 const rightArrow = document.getElementById("arrow-right")
+const exitButton = document.getElementById("exit-button")
 
 
+productCard.addEventListener("mouseover", cardHover);
+productCard.addEventListener("mouseleave", cardReset);
 flipButton.addEventListener("click", flipCard);
+
 exitButton.addEventListener("click", flipBack);
+
 rightArrow.addEventListener("click", slideRight);
 leftArrow.addEventListener("click", slideLeft);
 
@@ -37,13 +45,24 @@ function slideLeft() {
     }
 }
 
-function flipBack() {
-    cardToFlip.style.transform = "rotateY(0deg)";
-    imageContainer.style.transform = "translateX(0px)";
-    currentPosition = 1;
-}
+
 
 function flipCard() {
     cardToFlip.style.transform = "rotateY(180deg)";
 }
 
+function flipBack() {
+    cardToFlip.style.transform = "rotateY(0deg)";
+}
+
+
+function cardHover() {
+    infoCard.classList.add("card-move");
+    infoCard.style.overflow = "visible";
+    
+}
+
+function cardReset() {
+    infoCard.classList.remove("card-move");
+    infoCard.style.overflow = "hidden";
+}
