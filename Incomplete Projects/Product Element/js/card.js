7b8e5b04b25133c051b41907791800974768c424
gedizzletsplay@gmail.com <Duke1@234>
2022-05-29 02:54:44 +0100

    slider is functional

--- Incomplete Projects/Product Element/js/card.js
+++ Incomplete Projects/Product Element/js/card.js
@@ -1,26 +1,49 @@
-const productCard = document.querySelector(".product-card")
-const infoCard = document.getElementById("card-info")
-const sizesColors = document.getElementById("pop-up")
-
 const cardToFlip = document.querySelector(".flip-card")
 const flipButton = document.getElementById("flip-button")
+const exitButton = document.getElementById("exit-button")
 
-productCard.addEventListener("mouseover", cardHover);
-productCard.addEventListener("mouseleave", cardReset);
+const imageContainer = document.querySelector(".image-container")
+
+const leftArrow = document.getElementById("arrow-left")
+const rightArrow = document.getElementById("arrow-right")
+
+
 flipButton.addEventListener("click", flipCard);
+exitButton.addEventListener("click", flipBack);
+rightArrow.addEventListener("click", slideRight);
+leftArrow.addEventListener("click", slideLeft);
+
+var currentPosition = 1;
+
+function slideRight() {
+    if (currentPosition == 1) {
+        imageContainer.style.transform = "translateX(-333px)";
+        currentPosition += 1;
+    } else if (currentPosition == 2) {
+        imageContainer.style.transform = "translateX(-666px)";
+        currentPosition += 1;
+
+    }
+
+}
+
+function slideLeft() {
+    if (currentPosition == 2) {
+        imageContainer.style.transform = "translateX(0px)";
+        currentPosition -= 1;
+    } else if (currentPosition == 3) {
+        imageContainer.style.transform = "translateX(-333px)";
+        currentPosition -= 1;
+    }
+}
+
+function flipBack() {
+    cardToFlip.style.transform = "rotateY(0deg)";
+    imageContainer.style.transform = "translateX(0px)";
+    currentPosition = 1;
+}
 
 function flipCard() {
     cardToFlip.style.transform = "rotateY(180deg)";
 }
 
-
-function cardHover() {
-    infoCard.classList.add("card-move");
-    infoCard.style.overflow = "visible";
-    
-}
-
-function cardReset() {
-    infoCard.classList.remove("card-move");
-    infoCard.style.overflow = "hidden";
-}
