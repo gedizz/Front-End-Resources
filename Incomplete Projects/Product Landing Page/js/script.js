d5b71ec1385f98936efd733ed040aaaac9fdac7b
gedizzletsplay@gmail.com <Duke1@234>
2022-05-30 20:51:07 +0100

    added galler image click functionality

--- Incomplete Projects/Product Landing Page/js/script.js
+++ Incomplete Projects/Product Landing Page/js/script.js
@@ -21,14 +21,71 @@ const images = ["images/beer1.jpg",
   "images/beer3.jpg",
   "images/beer4.jpg"];
 
+// Change image on clicking of gallery image
+var animationStopped = 0;
+galleryImages = document.querySelector(".gallery").children;
+
+for (const image of galleryImages) {
+  image.addEventListener("click", changeImage);
+}
+
+function changeImage(event) {
+  newImage = event.path[0].getAttribute("src");
+  animationStopped = 1;
+  currentImage.src = newImage;
+}
+
+
 /* initialize all dynamic content */
 currentImage.src = images[currentIndex];
 console.log(images.length);
 
-window.setInterval(() => { 
-  currentImage.animate(imageAnimationClear, imageAnimationTiming);
-  currentImage.src = images[++currentIndex % images.length]; 
-  window.setTimeout(() => {
-    currentImage.animate(imageAnimationBlur, imageAnimationTiming);
-  }, 4000)
-}, 5000); 
+window.setInterval(() => {
+  if (!animationStopped) {
+    currentImage.animate(imageAnimationClear, imageAnimationTiming);
+    currentImage.src = images[++currentIndex % images.length];
+    window.setTimeout(() => {
+      currentImage.animate(imageAnimationBlur, imageAnimationTiming);
+    }, 4000)
+  }
+
+}, 5000);
+
+
+
+
+
+
+
+
+// Cart Quantity Selector
+const quantityUp = document.getElementById("quantity-up");
+const quantityDown = document.getElementById("quantity-down");
+const quantityInput = document.getElementById("quantity");
+
+currentQuantity = quantityInput.value;
+// get stock from db later
+var stock = 10;
+
+quantityUp.addEventListener('click', raiseQuantity);
+quantityDown.addEventListener('click', lowerQuantity);
+
+function raiseQuantity() {
+  if (currentQuantity < stock) {
+    ++currentQuantity;
+    quantityInput.value = currentQuantity;
+  } else {
+    console.log("Not enough stock")
+  }
+}
+
+function lowerQuantity() {
+  if (currentQuantity > 1) {
+    --currentQuantity;
+    quantityInput.value = currentQuantity;
+  } else {
+    console.log("Cant go down to 0")
+  }
+}
+
+
